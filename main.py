b0ef99d659942021bd4e33dbb24eab4d9a14949e
siegfriedgreg <gsiegfri@eagles.nccu.edu>
2021-08-19 20:16:10 -0400

    Refactored program with updates for newer API standards from OpenWeatherMap.org; not complete but parts are working

--- main.py
+++ main.py
@@ -1,125 +1,55 @@
-#==================================================================================
+#================================================================================
 ##
 #
 #    Author: Greg Siegfried
 #    Date: 5/16/2020
 #    Project: FlyToday App in Python
 #
-#    Purpose: using a user supplied api key, zip code, and desired environment
+#    Purpose: using a us supplied api key, zip code, and desired environment
 #            variables, the program will get the local weather information from
 #            'openweathermap.org'; and based on that information and the wuer
 #            defined environment variables, will suggest if it is a good day
 #            to fly or not. 
 #
-#    Features: Supports current weather information. Working on the forcast method
-#            to be able to give you days and times to expect a good flight.
+#    Features: Supports current weather information. Working on the forcast 
+#          methodto be able to give you days and times to expect a good flight.
 #
 ##
 ####
 
-import json
-import requests
-from info_data import Weather as wu
-from module1 import User as us
+from user_data import User as us
 
 ################################################################################
 ####------------>>>>            MAIN PROGRAM
 ################################################################################
 
-# Old Version of program/
-def mainWeather():
-    #### PART 1: GET AND SET WEATHER RESPONSE
-    with requests.get(wu.urlWeather(wu)) as rx:
-        kx = rx.json()
-    #### PART 2: IF VALID RESPONSE WRITE TO FILE
-    if(kx["cod"] != 404):
-        with open(wu.fileName, 'w') as output:
-            json.dump(kx, output, sort_keys=True, indent=2)
-    #### PART 3: ELSE wuE LAST SAVED RESPONSE
-    else:
-        print("\t -- Invalid Response, wuing Last Known Values -- \n")
-    #### PART 4: OPEN SAVED FILE
-    with open(wu.fileName, 'r') as input:
-        kx = json.load(input)
-    #### PART 5: SET RESPONSE VALUES
-    try:
-        # CLOUDS
-        wu.evalClouds(kx["clouds"])
-        wu.parseClouds(kx["clouds"])
-        # DT
-        wu.parseDT(kx["dt"])
-        # MAIN
-        wu.evalTemp(kx["main"])
-        wu.parseTemp(kx["main"])
-        # SYS
-        wu.parseSys(kx["sys"])
-        # VISIBILITY
-        wu.parseVis(kx["visibility"])
-        # WEATHER
-        wu.evalWeather(kx["weather"])
-        wu.parseWeather(kx["weather"])
-        # WIND
-        wu.evalWind(kx["wind"])
-        wu.parseWind(kx["wind"])
-    except:
-        print("CDMSWW Not Found")
-    #### PART 6: PRINT FORMATTED DATA
-    wu.printReport()
-    #### PART 7: TEST RESPONSES AND PRINT MESSAGE
-    fly = wu.testCheck(wu)
-    wu.printCheck(fly)
-
-
-# New Version of Program\
-def updateForcast():
-    x = us.getURL()
-    with requests.get(x) as rx:
-        kx = rx.json( )
-
-    with open(us._fileName, 'w') as output:
-        json.dump(kx, output, sort_keys=True, indent=2)
-
-def parseCurrentWeather():
-    with open(us._fileName, 'r') as input:
-        kx = json.load(input)
-        try:
-            us.parseCurrent( kx["current"], kx["daily"][0] )
-        except Exception as e:
-            print(e)
-
-def parseTomorrowsWeather():
-    with open(us._fileName, 'r') as input:
-        kx = json.load(input)
-        try:
-            us.parseNextDay( kx["daily"][1] )
-        except Exception as e:
-            print(e)
-
-def parseWeekOut():
-    with open(us._fileName, 'r') as input:
-        kx = json.load(input)
-        try:
-            us.parseWeekOutlook( kx["daily"] )
-        except Exception as e:
-            print(e)
-
-def parse3DaysOut():
-    with open(us._fileName, 'r') as input:
-        kx = json.load(input)
-        try:
-            us.parse3DayOutlook( kx["daily"], kx["hourly"] )
-        except Exception as e:
-            print(e)
-
-
-# RUN NEW VERSION
+# RUN MAIN
 if __name__ == "__main__":
-    updateForcast()
-    parseCurrentWeather()
-    parseTomorrowsWeather()
-    parseWeekOut()
-    parse3DaysOut()
 
+    # Create User
+    md = us()
+    repeat = True
+
+    while repeat:
+
+        print(" Please choose a display option: ")
+        print("\t Current Weather = 1 ")
+        print("\t 3 Day Outlook = 2 ")
+        print("\t 5 Day Outlook = 3 ")
+        print("\t Quit Program! = 9 ")
+
+        input_var = input("Choice:  ")
+
+        if input_var == '1':
+            md.current_weather()
+        elif input_var == '2':
+            md.three_days_out()
+        elif input_var == '3':
+            md.week_out()
+        elif input_var == '9':
+            repeat = False
+        else:
+            print("---- Please Input A Valid Choice! ----\n")
 
 ################################################################################
 ####                             END PROGRAM                                ####
---
+++ user_data.py
@@ -0,0 +1,199 @@
+# -------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------
+
+import json
+import requests
+import os.path as osp
+from time import localtime, strftime, time
+
+
+class User:
+
+    def __init__(self):
+
+        self.backup = "_backup.txt"
+        self.api_key = "b65e161b18736e911c299c20fd7858cc"
+        self.zip_code = "27707"
+        self.option = ['weather', 'forecast']
+        self.units = "imperial"
+        self.link = "http://api.openweathermap.org/data/2.5/"
+        self.desired = {'temp_min': 50, 'temp_max': 99,
+                        'wind_max': 22, 'clouds': 83}
+        self.weather_id = {800: "no clouds", 801: "few clouds",
+                           802: "scattered clouds", 803: "broken clouds",
+                           804: "overcast clouds"}
+
+    # Create the specific Weather/Forcast link to get the correct information
+    def update_file(self, args=0):
+
+        # Create a list of the required Open Weather API variables.
+        s_list = [self.link, self.option[args],
+                  self.zip_code, self.units, self.api_key]
+        # Create a formatted string with list data.
+        html_string = '{0[0]}{0[1]}?zip={0[2]}&units={0[3]}&appid={0[4]}'.format(
+            s_list)
+        # Create the file string depending on Weather/Forcast variable
+        file_string = self.option[args] + self.backup
+
+        # Request the API data for the file_name
+        with requests.get(html_string) as rx:
+            kx = rx.json()
+        # Write the new data to the file_name
+        with open(file_string, 'w') as output:
+            json.dump(kx, output, sort_keys=True, indent=2)
+
+    # Convert the wind_degree value into a wind direction icon.
+    # The pictogram is meant to illustrate the direction of travel with
+    # keyboard characters.
+    def convert_wind(self, value):
+
+        if (22 <= value <= 68):
+            return "NE"
+        elif (69 <= value <= 114):
+            return "E << "
+        elif (115 <= value <= 160):
+            return "SE"
+        elif (161 <= value <= 206):
+            return "S ^^ "
+        elif (207 <= value <= 252):
+            return "SW"
+        elif (253 <= value <= 298):
+            return "W >> "
+        elif (299 <= value <= 344):
+            return "NW"
+        else:
+            return "N vv "
+
+    # Convert from epoch time to a local date format
+    def convert_date(self, value):
+
+        return strftime("%a, %B %d ", localtime(value))
+
+    # Convert from epoch time to a local time format 12hr.
+    def convert_time(self, value):
+
+        return strftime("%I:%M:%S %p", localtime(value))
+
+    # Print out the current weather formatted data.
+    def print_current_day(self, args):
+
+        # Load Variables
+        lt = [self.convert_date(args['dt']), self.convert_time(args['dt'])]
+        dl = [self.convert_time(args['sys']["sunrise"]),
+              self.convert_time(args['sys']["sunset"])]
+        ltemp = [args['main']['temp'], args['main']['feels_like']]
+        rtemp = [args['main']['temp_min'], args['main']['temp_max']]
+        wind = [args['wind']['speed'], self.convert_wind(args['wind']['deg'])]
+        vis = [round(args["visibility"] / 5280, 2)]
+        desc = [args["weather"][0]["main"], args["weather"][0]["description"]]
+
+        # Print Variables
+        print("          Current Weather: ")
+        print('    Date: {0[0]}        Time: {0[1]}'.format(lt))
+        print('    Sunrise: {0[0]}      Sunset: {0[1]}'.format(dl))
+        print(
+            '    Current Temp: {0[0]} F     Feels Like: {0[1]} F'.format(ltemp))
+        print(
+            '    Minimum Temp: {0[0]} F     Maximum Temp: {0[1]} F'.format(rtemp))
+        print(
+            '    Wind Speed: {0[0]} mph      Wind Direction: {0[1]}'.format(wind))
+        print('    Visibility: {0[0]} mi     '.format(vis))
+        print('    Description: {0[0]} : {0[1]}'.format(desc))
+        print("\n")
+
+    # Print the hourly outlook for the next 3 days.
+    def print_three_day_outlook(self, args):
+
+        print("          3 Day Outlook: ")
+
+        # Get Valid Sunrise/Sunset values for good weather for the next 3 days.
+        for i in range(1, 4):
+            print('  {}:  '.format(self.convert_date(args[i]["dt"])))
+
+            # Filter by weatherID and Cloud values.
+            if(args[i]["weather"][0]["id"] in self.weather_id
+               and args[i]["clouds"] <= self.desired["clouds"]):
+
+                # Take Filtered Value and Print the Hours within Sunrise to Sunset.
+                for j in range(0, 47):
+
+                    if(args[i]["sunrise"] <= args[j]["dt"] <= args[i]["sunset"]):
+
+                        print('  Time: {}  Temp: {}  Clouds: {}  Wind: {}  Weather: {} '.format(
+                              User.convert_time(
+                                  args[j]["dt"]), args[j]["temp"], args[j]["clouds"],
+                              args[j]["wind_speed"], args[j]["weather"][0]["description"]))
+ 
+    # Print the outlook for the next week.
+    def print_week_outlook(self, args):
+
+        print("          Weekly Outlook: ")
+
+        for i in args:
+
+            if(i["weather"][0]["id"] in self.weather_id and i["clouds"]["all"] <= self.desired["clouds"]):
+                print('    :GOOD:  {} : {}; clouds: {}'.format(
+                    self.convert_date(i["dt"]), i["weather"][0]["description"], i["clouds"]["all"]))
+
+            elif i["weather"][0]["id"] in self.weather_id:
+                print('    :MAYBE: {} : {}; clouds: {}'.format(
+                    self.convert_date(i["dt"]), i["weather"][0]["description"], i["clouds"]["all"]))
+
+            else:
+                print('    :BAD:   {} : {}; clouds: {}'.format(
+                    self.convert_date(i["dt"]), i["weather"][0]["description"], i["clouds"]["all"]))
+
+        print("\n")
+
+    # Current Weather function
+    def current_weather(self):
+
+        delta_tee = 0
+        file_string = self.option[0] + self.backup
+
+        try:
+            delta_tee = time() - osp.getmtime(file_string)
+        except OSError:
+            self.update_file(0)
+        finally:
+            if delta_tee >= 600.1:
+                self.update_file(0)
+
+        with open(file_string, 'r') as input:
+            kx = json.load(input)
+            try:
+                self.print_current_day(kx)
+            except Exception as e:
+                print(e)
+
+    # Three Day Outlook Function
+    def three_days_out(self):
+
+        file_string = self.option[1] + self.backup
+
+        with open(file_string, 'r') as input:
+            kx = json.load(input)
+            try:
+                self.print_three_day_outlook(kx["list"])
+            except Exception as e:
+                print(e)
+
+    # Week Outlook Function
+    def week_out(self):
+
+        file_string = self.option[1] + self.backup
+
+        with open(file_string, 'r') as input:
+            kx = json.load(input)
+            try:
+                self.print_week_outlook(kx["list"])
+            except Exception as e:
+                print(e)
+
+# -------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------
